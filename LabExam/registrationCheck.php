473d0ee8f9234bd4873d18c88b35111349c43517
Saiful Alam <107752127+Abik67@users.noreply.github.com>
2024-11-25 13:10:37 +0600

    1st commit

---
+++ LabExam/registrationCheck.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $dob = $_POST['dob'];
+    $gender = $_POST['gender'];
+    $department = $_POST['department'];
+    $address = $_POST['address'];
+
+    if (!isset($_SESSION['users'])) {
+        $_SESSION['users'] = [];
+    }
+
+    $user_data = [
+        'name' => $name,
+        'email' => $email,
+        'password' => $password,
+        'dob' => $dob,
+        'gender' => $gender,
+        'department' => $department,
+        'address' => $address
+    ];
+
+    $_SESSION['users'][$name] = $user_data;
+
+    $_SESSION['user'] = $user_data;
+
+    $_SESSION['status'] = true;
+
+    header('Location: home.php');
+    exit();
+} else {
+    echo "Invalid Request!";
+}
+?>
